0cb11733cae1003d6e9794028a954f51a8fd8bca
Ihor Chyzh <ichyzh@gmail.com>
2017-02-12 22:33:20 +0200

    Add some homework, lesson8. The first task of homework is in lesson 7.

---
+++ src/main/java/com/ihorchyzh/app/homework/lesson8/Lesson8.java
@@ -0,0 +1,47 @@
+package com.ihorchyzh.app.homework.lesson8;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by ihorchyzh on 2/12/17.
+ */
+public class Lesson8 {
+
+    ///   5 строк по 8 столбцов в каждой из случайных целых чисел из отрезка [-99;99]
+
+    public int[][] multidimensionalArray(int rows, int columns, int startElement, int endElement) {
+        int[][] multidimensionalArray = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                multidimensionalArray[i][j] = ThreadLocalRandom.current()
+                        .nextInt(startElement, endElement + 1);
+            }
+        }
+        return multidimensionalArray;
+    }
+
+    public int minMultidimensionalArrayValue(int[][] array) {
+        int minimum = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (minimum > array[i][j]) {
+                    minimum = array[i][j];
+                }
+            }
+        }
+        return minimum;
+    }
+
+    public int maxMultidimensionalArrayValue(int[][] array) {
+        int maximum = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (maximum < array[i][j]) {
+                    maximum = array[i][j];
+                }
+            }
+        }
+        return maximum;
+    }
+
+}
--- src/main/java/com/ihorchyzh/runners/classwork/lesson7/Lesson7Runner.java
+++ src/main/java/com/ihorchyzh/runners/classwork/lesson7/Lesson7Runner.java
@@ -14,13 +14,21 @@ public class Lesson7Runner {
 
         Lesson7 lesson7 = new Lesson7();
 
-//        int[] array = {5, 1, 3, 2, 4};
-//
-//        System.out.println(Arrays.toString(array));
-//        System.out.println(Arrays.toString(lesson7.sortArray(array)));
-//        System.out.println(Arrays.toString(lesson7.baubleSortSmallToBig(array)));
+        int[] array = {5, 1, 3, 2, 4};
+        int arraySize = 80;
 
-        System.out.println(Arrays.toString(lesson7.charArray(300)));
+        System.out.println("baubleSortSmallToBig ver 1\n" +
+                Arrays.toString(lesson7.baubleSortSmallToBig1(array)) + "\n");
+        System.out.println("baubleSortBigToSmall ver 1\n" +
+                Arrays.toString(lesson7.baubleSortBigToSmall1(array)) + "\n");
+        System.out.println("baubleSortSmallToBig ver 2\n" +
+                Arrays.toString(lesson7.baubleSortSmallToBig2(array)) + "\n");
+        System.out.println("baubleSortBigToSmall ver 2\n" +
+                Arrays.toString(lesson7.baubleSortBigToSmall2(array)) + "\n");
+        System.out.println("standardSort\n" +
+                Arrays.toString(lesson7.sortArray(array)) + "\n");
+        System.out.println("outCharArray\n" +
+                Arrays.toString(lesson7.charArray(arraySize)));
     }
 
 }
