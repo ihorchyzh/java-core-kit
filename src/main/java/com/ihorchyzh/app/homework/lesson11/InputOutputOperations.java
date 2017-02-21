b6bf9634fe12534cacde2267cbdc52ab75806a34
Ihor Chyzh <ichyzh@gmail.com>
2017-02-21 23:07:26 +0200

    Add half working half of the home work.

--- src/main/java/com/ihorchyzh/app/homework/lesson11/InputOutputOperations.java
+++ src/main/java/com/ihorchyzh/app/homework/lesson11/InputOutputOperations.java
@@ -1,14 +1,19 @@
 package com.ihorchyzh.app.homework.lesson11;
 
 import java.io.*;
+import java.util.Arrays;
 
 /**
- * Created by Igor Chyzh on 2/21/2017.
+ * Created by Ihor Chyzh on 2/21/2017.
  */
 public class InputOutputOperations {
 
-    private final String inputArraysForSortFile = "src\\test\\resources\\lesson11Data\\inputArraysForSort.txt";
-    private final String outputSortedArraysFile = "src\\test\\resources\\lesson11Data\\outputSortedArrays.txt";
+    private final String inputArraysForSortFile = "src/main/resources/lesson11Data/inputArraysForSort.txt";
+    private final String outputSortedArraysFile = "src/main/resources/lesson11Data/outputSortedArrays.txt";
+    private final String isPalindromInputFile = "src/main/resources/lesson11Data/isPalindromInput.txt";
+    private final String isPalindromOutputFile = "src/main/resources/lesson11Data/isPalindromOutput.txt";
+
+//    Task 1
 
     public String readFile() {
         StringBuilder sb = new StringBuilder();
@@ -17,7 +22,8 @@ public class InputOutputOperations {
             BufferedReader reader = new BufferedReader(
                     new FileReader(inputArraysForSortFile));
             while ((line = reader.readLine()) != null) {
-                sb.append(line);
+                if (!line.equals(null) || !line.isEmpty())
+                    sb.append(line);
             }
             reader.close();
         } catch (FileNotFoundException e) {
@@ -25,7 +31,7 @@ public class InputOutputOperations {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return sb.append(line).toString();
+        return sb.toString();
     }
 
     public void writeFile(int[] array) {
@@ -34,7 +40,7 @@ public class InputOutputOperations {
             BufferedWriter writer = new BufferedWriter(
                     new OutputStreamWriter(
                             new FileOutputStream(outputSortedArraysFile)));
-            writer.append(result);
+            writer.write(Arrays.toString(array));
             writer.newLine();
             writer.close();
         } catch (FileNotFoundException e) {
@@ -46,7 +52,7 @@ public class InputOutputOperations {
 
     public int[] stringToIntArray(String input) {
         String[] stringArray = input
-                .replaceAll("[\\s+|\\[|\\]|\\{|\\}]", "")
+                .replaceAll("[\\s|\\[|\\]|\\{|\\}]", "")
                 .trim()
                 .split(",");
         int[] intArray = new int[stringArray.length];
@@ -57,8 +63,8 @@ public class InputOutputOperations {
     }
 
     public int[] sortArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int tmp = array[j];
                     array[j] = array[j + 1];
@@ -68,5 +74,38 @@ public class InputOutputOperations {
         }
         return array;
     }
+
+//    Task 2
+//      Реализовать программу, которая считывает из файла список слов в стобик,
+//      и выводит в другой файл список слов и признак полиндромности
+//      в виде “moom is polindrome”.
+
+    public void isPalindromeInFile() {
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(isPalindromInputFile));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(isPalindromOutputFile)));
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals(null) || !line.isEmpty()) {
+                    if ((line.trim()).equalsIgnoreCase((sb.append(line).reverse()).toString())) {
+                        writer.write((sb.append(" is palindrome!")).toString());
+                        writer.newLine();
+                    } else {
+                        writer.write((new StringBuilder(line).append(" is not palindrome!")).toString());
+                        writer.newLine();
+                    }
+                }
+            }
+            reader.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
--- src/main/java/com/ihorchyzh/runners/homework/Lesson11RunnerHome.java
+++ src/main/java/com/ihorchyzh/runners/homework/Lesson11RunnerHome.java
@@ -1,13 +1,17 @@
 package com.ihorchyzh.runners.homework;
 
+import com.ihorchyzh.app.homework.lesson11.InputOutputOperations;
+
 /**
- * Created by Igor Chyzh on 2/21/2017.
+ * Created by Ihor Chyzh on 2/21/2017.
  */
 public class Lesson11RunnerHome {
 
     public static void main(String[] args) {
 
-
+        InputOutputOperations io = new InputOutputOperations();
+        io.writeFile(io.sortArray(io.stringToIntArray(io.readFile())));
+        io.isPalindromeInFile();
 
     }
 
